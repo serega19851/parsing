379d58bb7fa92378d0974a68b62e27ba5ea30fed
Serega <antonovseantonoff@yandex.ru>
2023-03-22 12:32:35 +0300

    Fixed bugs

--- manage.py
+++ manage.py
@@ -1,66 +1,39 @@
-from pprint import pprint
 import requests
 from pathlib import Path
 from pathvalidate import sanitize_filename
 from bs4 import BeautifulSoup
 from requests import HTTPError
-from urllib.parse import urlparse, urljoin
+from urllib.parse import urljoin
 import argparse
 
 
-def creates_directory(name):
-    return Path(name).mkdir(parents=True, exist_ok=True)
-
-
-def creates_new_urls(ranging):
-    new_url = []
-    for number in ranging:
-        new_url.append(f"https://tululu.org/txt.php?id={number}")
-    return new_url
-
-
-def check_for_redirect(url, response):
-    if url != response.url:
+def check_for_redirect(response):
+    if response.is_redirect:
         raise HTTPError("Redirect detected. Download canceled")
 
 
-def get_except(url):
-    response = requests.get(url, allow_redirects=True)
-    try:
-        response.raise_for_status()
-        check_for_redirect(url, response)
-    except HTTPError as inf:
-        print("Unable to download file ", url, str(inf))
-        return ""
-    return response.url
-
-
 def gets_title(soup):
-    title = soup.find('h1').text.split("::")
-    return title[0].strip(), title[-1].strip()
+    title, author = soup.find('h1').text.split("::")
+    return title.strip(), author.strip()
 
 
-def download_txt(url, filename, folder='books/'):
-    creates_directory(folder)
-    response = requests.get(url, allow_redirects=True)
-    response.raise_for_status()
+def download_txt(response, filename, folder='books/'):
+    Path(folder).mkdir(parents=True, exist_ok=True)
     path = f'{folder}{sanitize_filename(filename)}.txt'
     with open(path, 'wb') as file:
         file.write(response.content)
     return path
 
 
-def display_link_cover_book(soup, book_url):
+def get_url_cover_book(soup, book_url):
     book_cover = soup.find('div', class_='bookimage').find('img')['src']
     return urljoin(book_url, book_cover)
 
 
-def download_images(soup, book_url, folder='images/'):
-    creates_directory(folder)
-    filename = urlparse(
-        display_link_cover_book(soup, book_url)
-    ).path.split("/")[-1]
-    response = requests.get(display_link_cover_book(soup, book_url))
+def download_images(url_cover, folder='images/'):
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    filename = url_cover.split('/')[-1]
+    response = requests.get(url_cover)
     response.raise_for_status()
     path = f'{folder}{sanitize_filename(filename)}'
     with open(path, 'wb') as file:
@@ -69,27 +42,25 @@ def download_images(soup, book_url, folder='images/'):
 
 def gets_comments(soup):
     comments = soup.find_all('div', class_='texts')
-    commentaries = []
-    for comment in comments:
-        commentaries.append(comment.find('span').text)
+    commentaries = [comment.find('span').text for comment in comments]
     return commentaries
 
 
-def gets_book_genre(soup):
-    genre = soup.find('span', class_='d_book').text.split(":")[-1].strip()
-    return genre
+def gets_book_genres(soup):
+    genres = soup.find('span', class_='d_book').text.split(":")[-1].strip()
+    return genres
 
 
-def parse_book_page(soup, book_url, url):
-    book_info = {
-        'title': gets_title(soup)[0],
-        'author': gets_title(soup)[1],
-        'genre': gets_book_genre(soup),
+def parse_book_page(soup, book_url, url, title, author):
+    book_page = {
+        'title': title,
+        'author': author,
+        'genres': gets_book_genres(soup),
         'comments': gets_comments(soup),
-        'cover_book': display_link_cover_book(soup, book_url),
+        'cover_book': get_url_cover_book(soup, book_url),
         'download_link': url,
     }
-    pprint(book_info)
+    return book_page
 
 
 def create_parser():
@@ -101,21 +72,42 @@ def create_parser():
         "--end_id", type=int, help="enter the number", default=5
     )
     args = parser.parse_args()
-    ranging = range(args.start_id, args.end_id + 1)
-    return ranging
+    return args.start_id, args.end_id
 
 
 def main():
-    for url in creates_new_urls(create_parser()):
-        if get_except(url):
-            book_id = urlparse(url).query.split('=')[-1].strip()
-            book_url = urljoin(url, f'b{book_id}/')
-            response = requests.get(book_url)
+    start_id, end_id = create_parser()
+    for number in range(start_id, end_id + 1):
+        try:
+            params = {'id': number}
+            response = requests.get(
+                "https://tululu.org/txt.php",
+                params=params,
+                allow_redirects=False
+            )
             response.raise_for_status()
-            soup = BeautifulSoup(response.text, 'lxml')
-            parse_book_page(soup, book_url, url)
-            download_txt(url, gets_title(soup)[0])
-            download_images(soup, book_url)
+            book_url = urljoin(response.url, f'b{number}/')
+
+            book_response = requests.get(book_url)
+            book_response.raise_for_status()
+            soup = BeautifulSoup(book_response.text, 'lxml')
+
+            check_for_redirect(response)
+            title, author = gets_title(soup)
+            book_page = parse_book_page(
+                soup,
+                book_url,
+                response.url,
+                title,
+                author
+            )
+            download_images(book_page['cover_book'])
+            download_txt(response, title)
+
+        except HTTPError as inf:
+            print("Unable to download file ", response.url, str(inf))
+        except requests.exceptions.ConnectionError as errc:
+            print("Error Connecting:", errc)
 
 
 if __name__ == '__main__':
