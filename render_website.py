2b2ae05484b4372ae65a73bbe4a123af44bc96a7
Serega <antonovseantonoff@yandex.ru>
2023-04-13 14:55:44 +0300

    Chose the main type of quotes

--- render_website.py
+++ render_website.py
@@ -10,8 +10,8 @@ from more_itertools import chunked
 def gets_args():
     parser = argparse.ArgumentParser('accepts optional args')
     parser.add_argument(
-        "-jp", "--json_path",
-        help="in enter your path to the file",
+        '-jp', '--json_path',
+        help='in enter your path to the file',
         default=Path.cwd()
     )
     args = parser.parse_args()
@@ -21,8 +21,8 @@ def gets_args():
 def on_reload(json_path, books_number_per_page):
     number_of_columns = 2
     Path('pages').mkdir(parents=True, exist_ok=True)
-    path_json = os.path.join(json_path, "book_page.json")
-    with open(path_json, "r") as file:
+    path_json = os.path.join(json_path, 'book_page.json')
+    with open(path_json, 'r') as file:
         book_descriptions = list(chunked(json.load(file), books_number_per_page))
     number_pages = len(book_descriptions)
     for num, page_with_books in enumerate(book_descriptions, 1):
@@ -31,7 +31,7 @@ def on_reload(json_path, books_number_per_page):
             loader=FileSystemLoader('.'),
             autoescape=select_autoescape(['html'])
         )
-        template = env.get_template(os.path.join('templates', "template.html"))
+        template = env.get_template(os.path.join('templates', 'template.html'))
         rendered_page = template.render(
             books_part=books_part,
             number_pages=number_pages,
@@ -39,7 +39,7 @@ def on_reload(json_path, books_number_per_page):
         )
         with open(os.path.join(
                 'pages', f'index{num}.html'
-        ), 'w', encoding="utf8") as file:
+        ), 'w', encoding='utf8') as file:
             file.write(rendered_page)
 
 
